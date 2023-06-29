427e5b64d8c9c39d3aa906ea396b88c3b987d18c
Mananchovatiya <mananchovatiya15@gmail.com>
2023-06-29 09:04:13 +0530

    29-06-2023

---
+++ oops/result-constructor.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class test
+{
+    int m,s,e,total;
+    float per;
+    public:
+    test()
+    {      
+            cout<<"\nEnter m: ";
+            cin>>m;
+            cout<<"\nEnter s: ";
+            cin>>s;
+            cout<<"\nEnter e: ";
+            cin>>e;
+    }
+    void r()
+    {
+        total=m+s+e;
+        per=total/3;
+    }
+    void a()
+    {
+        cout<<"m\ts\te\ttotal\tper\n";
+        cout<<m<<"\t"<<s<<"\t"<<e<<"\t"<<total<<"\t"<<per<<"\t";
+    }
+};
+int main()
+{
+    test x;
+    x.r();
+    x.a();
+}
